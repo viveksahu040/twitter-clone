07d9fea12996c9c427167b4a377e569a2259fef4
Vivek Sahu <sahuviveksahu2004@gmail.com>
2024-11-22 21:09:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cookieParser from 'cookie-parser';
 import dotenv from "dotenv";
@@ -19,6 +20,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve()
 
 app.use(express.json({limit:"2mb"}));
 
@@ -28,6 +30,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 app.listen(PORT,()=>{
